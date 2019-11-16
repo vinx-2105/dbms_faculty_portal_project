f78479c7ff61faece82e1bd0a54b6b8ae241e02d
vinx-2105 <2017csb1119@iitrpr.ac.in>
2019-11-16 22:19:28 +0530

    reorganized repo

--- src/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    $_SESSION['loggedin'] = false;
-    $_SESSION['username'] = NULL;
-    // $_SESSION['curr_doc']=NULL;
-    // $_SESSION["curr_collection"]=NULL;
-    header("Location: ./index.php");
-?>
