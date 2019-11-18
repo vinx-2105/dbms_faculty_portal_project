af9641d5abcad794f80b50fe40fcdcf33416bb0e
vinx-2105 <2017csb1119@iitrpr.ac.in>
2019-11-18 12:50:05 +0530

    created error.php

--- part2/logout.php
+++ part2/logout.php
@@ -1,9 +1,9 @@
 <?php
-    session_start();
     $_SESSION['loggedin'] = false;
     $_SESSION['username'] = NULL;
     $_SESSION['rank_title'] = NULL;
     $_SESSION['special_post'] = NULL;
     $_SESSION['post_id'] = NULL;
+    
     header("Location: ./login.php");
 ?>
