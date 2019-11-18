1bf89c411474541b28958c21078e524f4e0b1c8d
vinx-2105 <2017csb1119@iitrpr.ac.in>
2019-11-18 11:37:54 +0530

    updated accept_application

--- part2/send_after_review.php
+++ part2/send_after_review.php
@@ -7,17 +7,15 @@
     $transaction_id = $_POST['transaction_id'];
     $remarks = $_POST['remarks'];
 
-    echo $transaction_id."\n";
-    echo $remarks."\n";
-    // $transaction = get_leave_trans_by_id($db_connection, $transaction_id);
+    $transaction = get_leave_trans_by_id($db_connection, $transaction_id);
 
-    // $leave = get_leave_by_id($db_connection, $transaction['leave_id']);
+    $leave = get_leave_by_id($db_connection, $transaction['leave_id']);
 
-    // $faculty_id = $leave['faculty_id'];
+    $faculty_id = $leave['faculty_id'];
 
-    // $route_id = $transaction['route_id'];
-    // $route = get_route_by_id($db_connection, $route_id);
+    $route_id = $transaction['route_id'];
+    $route = get_route_by_id($db_connection, $route_id);
 
-    // $route_num_nodes = $route[0];
+    $route_num_nodes = $route[0];
 
 ?>
